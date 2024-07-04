b85439a3cbf433fcb7567d8662490ced16dceca1
leakna <phornleakna@gmail.com>
2024-07-04 09:32:38 +0700

    update

--- exercise9.js
+++ exercise9.js
@@ -1,13 +1,10 @@
 let isPrime=true;
 for(let i=1;i<=100;i++){
   isPrime=true;
-  for(let j=1;j<=i;j++){
-    if((j!=i&&i%j==0)&&j!=1)
+  for(let j=2;j<=i;j++){
+    if(j!=i&&i%j==0)
       isPrime=false
-    if(i==1){
-      isPrime=true
-    }
   }
-  if(isPrime)
+  if(isPrime&&i!=1)
     console.log(i)
 }
